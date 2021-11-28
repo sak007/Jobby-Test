249e7826c945cec3e8e822676fa7153c758b6552
Athithya <sri.athithya.kruth@gmail.com>
2021-11-28 17:44:18 -0500

    fixed flake8 issuses

--- test/Scraper/test_indeed_scraper.py
+++ test/Scraper/test_indeed_scraper.py
@@ -1,16 +1,14 @@
-from mock import ANY
-import mock
-from mock.mock import patch
-import os
 import requests
 
 from importlib.machinery import SourceFileLoader
 indeed_scraper = SourceFileLoader('indeed_scraper', 'code/Scraper/indeed_scraper.py').load_module()
 
-def test_indeed_scraper_working (mocker):
+
+def test_indeed_scraper_working(mocker):
     ans = indeed_scraper.get_jobs("Software Engineer", "Raleigh", 10, ['Java', 'Python', 'Coding'])
     print(ans)
     assert ans is not None
+
 
 def test_indeed_scraper_nonworking(mocker):
     mocker.patch.object(indeed_scraper, 'requests')
