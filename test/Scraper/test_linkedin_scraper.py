c0f03711f2576a9c17a10de8bf371332c58d7323
Athithya <sri.athithya.kruth@gmail.com>
2021-11-28 18:32:02 -0500

    added Linkedin scraper test

---
+++ test/Scraper/test_linkedin_scraper.py
@@ -0,0 +1,9 @@
+import requests
+
+from importlib.machinery import SourceFileLoader
+linkedin_scraper = SourceFileLoader('linkedin_scraper', 'code/Scraper/linkedin_scraper.py').load_module()
+
+def test_get_jobs(mocker):
+    jobs = linkedin_scraper.get_jobs("Software Engineer", "Raleigh", 10, ['Java', 'Python', 'Coding'])
+    assert jobs is not None
+    #assert False
