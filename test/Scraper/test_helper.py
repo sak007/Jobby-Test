145012e50783da819d3763ce9fdcd3792d1b3fe8
zedunaid <zunaid.s@somaiya.edu>
2021-11-29 11:48:19 -0500

    test for get_all_skills

--- test/Scraper/test_helper.py
+++ test/Scraper/test_helper.py
@@ -12,12 +12,44 @@ def test_db_connect(mocker):
 
 
 def test_get_all_skills(mocker):
-    mocker.patch("builtins.open", return_value=[])
-    mocker.patch.object(helper, 'json')
-    helper.json.load.return_value = json.loads('{"server_name" : "srijas.cd1pfgqpboiq.us-east-1.rds.amazonaws.com", "user_name":"admin", "password": "adminadmin", "db_name": "srijas", "linked_in_pwd":"SRIJASGMAILPWD"}')
+    skills = [('Java'), ('Python'), ('C++')]
+
+    class mock_connection:
+        def cursor(a):
+            return mock_cursor
+
+    class mock_cursor:
+        def execute(a):
+            pass
+
+        def fetchall():
+            return skills
+
+    mocker.patch.object(helper, 'db_connect', mock_connection)
     allskills = helper.get_all_skills()
-    print(allskills)
-    assert allskills is not None
+
+    for s in skills:
+        assert s[0] in allskills
+
+
+def test_get_all_skills_empty_list(mocker):
+    skills = []
+
+    class mock_connection:
+        def cursor(a):
+            return mock_cursor
+
+    class mock_cursor:
+        def execute(a):
+            pass
+
+        def fetchall():
+            return skills
+
+    mocker.patch.object(helper, 'db_connect', mock_connection)
+    allskills = helper.get_all_skills()
+
+    assert allskills == []
 
 
 def test_extract_skills_empty_description():
