137edb545871ea54166cf0417679cf9c2b7357bb
Subramanian Venkataraman <subvnkt@gmail.com>
2021-11-29 16:51:37 -0500

    Updated Web Pages with new name - JOBBY

--- code/Web_app/register.php
+++ code/Web_app/register.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="author" content="Group-32, Fall 2021">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>S.R.I.J.A.S - Login</title>
+  <title>JOBBY - Dashboard</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
 </head>
