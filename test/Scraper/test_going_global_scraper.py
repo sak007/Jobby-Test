8967bc5fdaba344906ce8ed51b0eeed1ab5b5651
Rithik <isrithikta@gmail.com>
2021-11-29 00:27:08 -0500

    Added tests for GoinGlobal

---
+++ test/Scraper/test_going_global_scraper.py
@@ -0,0 +1,6 @@
+from importlib.machinery import SourceFileLoader
+goinglobal_scraper = SourceFileLoader('going_global_scraper', 'code/Scraper/going_global_scraper.py').load_module()
+
+def test_get_job_goin_global(mocker):
+    final_result = goinglobal_scraper.get_jobs("Programmer", "", 5, ["Programmer", "Analytic", "Experience"])
+    assert final_result is not None
