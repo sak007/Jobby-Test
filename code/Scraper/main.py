73c4d83184fb3f2e2041417149c5daa4888355b5
Rithik <isrithikta@gmail.com>
2021-11-29 00:34:09 -0500

    Fixed the name error in main.py for GoinGlobal

--- code/Scraper/main.py
+++ code/Scraper/main.py
@@ -84,7 +84,7 @@ def generate_job_map(job_board_role_mp, all_skills):
             elif (jb == 'MONSTER'):
                 j = monster_scraper.get_jobs(rl[0], rl[1], 10, all_skills)
             elif (jb == 'GOINGLOBAL'):
-                j = scrapper_goingglobal.get_jobs(rl[0], rl[1], 10, all_skills)
+                j = going_global_scraper.get_jobs(rl[0], rl[1], 10, all_skills)
             elif (jb == 'SIMPLYHIRED'):
                 j = simplyhired_scraper.get_jobs(rl[0], rl[1], 10, all_skills)
             job_map[jb][rl] = j
